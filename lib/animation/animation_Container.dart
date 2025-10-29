3bf2876b73b61a79026f005c885468d79221dffa
Aniket Yadav <aniketyadavtech1@gmail.com>
2025-10-29 07:48:28 +0530

    first commit

---
+++ lib/animation/animation_Container.dart
@@ -0,0 +1,91 @@
+import 'package:animation/animation/timer.dart';
+import 'package:flutter/material.dart';
+
+class AnimationContainer extends StatefulWidget {
+  const AnimationContainer({super.key});
+
+  @override
+  State<AnimationContainer> createState() => _AnimationContainerState();
+}
+
+class _AnimationContainerState extends State<AnimationContainer> {
+  bool isExpanded = false;
+  bool isExpandedOpacity = true;
+  bool isAlign = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Animation"),
+        actions: [
+          IconButton(onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>TimerCountView()));
+          }, icon: Icon(Icons.timer))
+        ],
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isExpanded = !isExpanded;
+                });
+              },
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                curve: Curves.easeInOut,
+                width: isExpanded ? 300 : 120,
+                height: isExpanded ? 300 : 120,
+                color: isExpanded ? Colors.red : Colors.blue,
+                child: Center(child: Text('Hello')),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isExpandedOpacity = !isExpandedOpacity;
+                });
+              },
+              child: AnimatedOpacity(
+                opacity: isExpandedOpacity ? 1.0 : 0,
+                duration: Duration(seconds: 1),
+                curve: Curves.easeInOut,
+                child: Container(
+                  color: Colors.blue,
+                  width: 200,
+                  height: 200,
+                  child: Center(child: Text("text")),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isAlign = !isAlign;
+                });
+              },
+              child: AnimatedAlign(
+                duration: Duration(seconds: 1),
+                curve: Curves.easeInOut,
+                alignment: isAlign? Alignment.topCenter:Alignment.bottomCenter,
+                child: Container(
+                  color: Colors.blue,
+                  width: 200,
+                  height: 200,
+                  child: Center(child: Icon(Icons.ac_unit,color: Colors.white, size: 50)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/animation/timer.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TimerController extends GetxController {
+  RxInt seconds = 0.obs;
+  Timer? timer;
+  RxBool isRunning = false.obs;
+
+
+  void startTimer() {
+    if (timer != null && timer!.isActive) return;
+    isRunning.value = true;
+    timer = Timer.periodic(Duration(seconds: 1), (_) {
+      seconds.value++;
+    });
+  }
+
+
+  void stopTimer() {
+    timer?.cancel();
+    isRunning.value = false;
+  }
+
+
+  void resetTimer() {
+    timer?.cancel();
+    seconds.value = 0;
+    isRunning.value = false;
+  }
+
+  @override
+  void onClose() {
+    timer?.cancel();
+    super.onClose();
+  }
+}
+
+
+
+
+class TimerCountView extends StatelessWidget {
+  final TimerController controller = Get.put(TimerController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("GetX Smart Timer")),
+
+      body: Center(
+        child: Obx(() {
+          int days = controller.seconds.value ~/ (24 * 3600);
+          int hours = (controller.seconds.value % (24 * 3600)) ~/ 3600;
+          int minutes = (controller.seconds.value % 3600) ~/ 60;
+          int secs = controller.seconds.value % 60;
+
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "${_twoDigit(days)} : ${_twoDigit(hours)} : ${_twoDigit(minutes)} : ${_twoDigit(secs)}",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: controller.isRunning.value
+                        ? controller.stopTimer
+                        : controller.startTimer,
+                    child: Text(controller.isRunning.value ? "Stop" : "Start"),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: controller.resetTimer,
+                    child: Text("Reset"),
+                  ),
+                ],
+              ),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+  String _twoDigit(int n) => n.toString().padLeft(2, '0');
+}
